e72190c5cc4e67e10cc1bbd27ef0f13f7ef9b498
Zain Naeem <zain.naeem@student.lab.fi>
2023-12-23 21:26:20 +0200

    started working on menu using js

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+// Select DOM items
+const menuBtn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-Items');
+
+// Set initial menu state
